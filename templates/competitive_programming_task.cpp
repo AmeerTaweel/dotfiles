c0b7915faccfec747cc84d08cddef6ff43348cce
Ameer Taweel <ameertaweel2002@gmail.com>
2021-02-17 09:57:54 +0200

    Update C++ competitive programming file template

--- templates/competitive_programming_task.cpp
+++ templates/competitive_programming_task.cpp
@@ -5,10 +5,18 @@ g++ file.cpp -std=c++17 -O2 -Wall -o out
 
 using namespace std;
 
+#define intfss int_fast8_t
+#define intfs int_fast16_t
+#define intf int_fast32_t
+#define intfl int_fast64_t
+
+#define FAST_IO \
+ios::sync_with_stdio(0);\
+cin.tie(0);\
+cout.tie(0);
+
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	FAST_IO
 
 	return 0;
 }
