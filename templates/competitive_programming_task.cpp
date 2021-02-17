95eea72739f3f39e4a20c9ffd77647f587a46e47
Ameer Taweel <ameertaweel2002@gmail.com>
2021-02-17 10:52:41 +0200

    Fix error in FAST_IO definition

--- templates/competitive_programming_task.cpp
+++ templates/competitive_programming_task.cpp
@@ -10,13 +10,13 @@ using namespace std;
 #define intf int_fast32_t
 #define intfl int_fast64_t
 
-#define FAST_IO; \
+#define FAST_IO \
 ios::sync_with_stdio(0);\
 cin.tie(0);\
 cout.tie(0);
 
 int main(){
-	FAST_IO
+	FAST_IO;
 
 	return 0;
 }
