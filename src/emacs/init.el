5e1f0fbcf1b8823db87e0569a0daa33d861c9ff4
Ameer Taweel <ameertaweel2002@gmail.com>
2021-04-16 16:35:24 +0300

    Update Emacs init.el

--- src/emacs/init.el
+++ src/emacs/init.el
@@ -3,25 +3,24 @@
 ;;-------------------------------------------------------------------------------
 
 ;;-------------------------------------------------------------------------------
-;; ## Auto-created Block - Do not edit by hand
+;; ## Auto-Created Block - Do not edit by hand
 ;;-------------------------------------------------------------------------------
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" default))
- '(package-selected-packages
-   '(org-bullets forge evil-magit magit counsel-projectile projectile hydra general evil-collection evil doom-themes helpful counsel which-key use-package rainbow-delimiters ivy-rich)))
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(custom-safe-themes
+	 '("75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" default))
+  )
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
 
 ;;-------------------------------------------------------------------------------
 
@@ -36,4 +35,3 @@
 
 ;; Load theme
 (load "~/.emacs.d/lisp/theme")
-
