d01b746f007bc3953f53abb348fe373f1b983460
Ameer Taweel <ameertaweel2002@gmail.com>
2021-04-16 17:22:28 +0300

    Update and reformat Emacs settings.el

--- src/emacs/lisp/settings.el
+++ src/emacs/lisp/settings.el
@@ -2,6 +2,27 @@
 ;; # Emacs Settings
 ;;-------------------------------------------------------------------------------
 
+;;-------------------------------------------------------------------------------
+;; ## Startup
+;;-------------------------------------------------------------------------------
+
+(defun settings/display-startup-time ()
+  "Displays startup time and number of garbage collections done by Emacs"
+  (message "Emacs loaded in %s with %d garbage collections."
+		   (format "%.2f seconds"
+				   (float-time
+					 (time-subtract after-init-time before-init-time)))
+		   gcs-done))
+
+;; See startup time and number of garbage collections on startup
+(add-hook 'emacs-startup-hook #'settings/display-startup-time)
+
+;;-------------------------------------------------------------------------------
+
+;;-------------------------------------------------------------------------------
+;; ## Interface
+;;-------------------------------------------------------------------------------
+
 ;; Turn off splash screen
 ;; The scratch buffer will show on startup
 (setq inhibit-startup-message t)
@@ -14,6 +35,18 @@
 (tooltip-mode -1)
 ;; Disable menu-bar
 (menu-bar-mode -1)
+
+;; Enable the visible bell
+(setq visible-bell t)
+
+;; Disable line wrapping
+(setq-default truncate-lines t)
+
+;;-------------------------------------------------------------------------------
+
+;;-------------------------------------------------------------------------------
+;; ## Line Numbers
+;;-------------------------------------------------------------------------------
 
 ;; Show column number in status line
 (column-number-mode)
@@ -28,11 +61,11 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; Enable the visible bell
-(setq visible-bell t)
+;;-------------------------------------------------------------------------------
 
-;; Disable line wrapping
-(setq-default truncate-lines t)
+;;-------------------------------------------------------------------------------
+;; ## Backup
+;;-------------------------------------------------------------------------------
 
 ;; Change the directory where emacs saves backup files
 (setq backup-directory-alist `(("." . "/tmp/emacs")))
@@ -40,9 +73,25 @@
 ;; Change backup strategy to not ruin symlinks
 (setq backup-by-copying-when-linked t)
 
-;; Set the auth sources file location
-(setq auth-sources '("~/.authinfo"))
+;;-------------------------------------------------------------------------------
+
+;;-------------------------------------------------------------------------------
+;; ## Terminal
+;;-------------------------------------------------------------------------------
+
+;; Turn on Unicode support in term and shell modes
+(dolist (mode '(term-exec-hook
+				 shell-mode-hook))
+  (add-hook mode (lambda () (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+;;-------------------------------------------------------------------------------
+
+;;-------------------------------------------------------------------------------
+;; ## Others
+;;-------------------------------------------------------------------------------
 
 ;; Automatically reload files was modified by external program
 (global-auto-revert-mode 1)
 (diminish auto-revert-mode)
+
+;;-------------------------------------------------------------------------------
