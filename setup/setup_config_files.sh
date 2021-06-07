30323f12f12378ea44397d345a87e30765aadf6b
Ameer Taweel <ameertaweel2002@gmail.com>
2021-06-07 20:23:13 +0300

    Update the setup script with shellcheck

--- setup/setup_config_files.sh
+++ setup/setup_config_files.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-: '
+: "
 This is the main configuration setup script.
 
-It will go through the individual directories of the `src` directory, and link them to the suitable place.
+It will go through the individual directories of the 'src' directory, and link them to the suitable place.
 
-It determines the suitable place based on the contents of the `.setup` file in each directory.
+It determines the suitable place based on the contents of the '.setup' file in each directory.
 
-The format of each line in the `.setup` file is:
+The format of each line in the '.setup' file is:
 'SELECTOR' : 'SELECT_TYPE' : 'DIST_DIRECTORY'
 
-The selector is a file/directory selector that can be passed to the `find` command.
+
+The selector is a file/directory selector that can be passed to the 'find' command.
 
 The select_type is either f or d, to determine to what to apply the selector (files or directories).
 
-The dist_directory is the place where all files/directories the `find` matches with the selector
+The dist_directory is the place where all files/directories the 'find' matches with the selector
 will be linked to.
-'
+
+Also, putting a # at the start of the line will make it a comment.
+"
 
 # Constants
 tab="    "
@@ -44,11 +47,11 @@ setup_dir=$root_dir/setup
 
 # Print directory info
 # Useful for debugging
-echo -e $yellow $bold
+echo -e "$yellow$bold"
 echo "+-------------------+"
 echo "| Base directories: |"
 echo "+-------------------+"
-echo -e $reset
+echo -e "$reset"
 echo
 echo -e "${blue}${bold}Root directory:${reset} $root_dir"
 echo -e "${blue}${bold}src directory:${reset} $src_dir"
@@ -68,16 +71,17 @@ if [[ ! -d $setup_dir ]]; then
 fi
 
 # Get a list of all files and directories under `src`, except for . and ..
-src_entries=$(ls $src_dir -A)
+src_entries=$(ls "$src_dir" -A)
 
-echo -e $yellow $bold
+echo -e "$yellow$bold"
 echo "+----------------------------------------+"
 echo "| Iterating through the src directory... |"
 echo "+----------------------------------------+"
-echo -e $reset
+echo -e "$reset"
 echo
 
 setup_line_regex="^'(.+)' : '(d|f)' : '(.+)'$"
+comment_line_regex="^#.*$" # Comments in .setup files
 for entry in $src_entries; do
 	# For each directory run the script recursively
 	if [[ -d $src_dir/$entry ]]; then
@@ -87,7 +91,7 @@ for entry in $src_entries; do
 			echo -e "${tab}${cyan}${bold}.setup${reset} file found. Parsing..."
 
 			# Parse line by line
-			while read line; do
+			while read -r line; do
 				# If line matches regex, continue linking
 				if [[ $line =~ $setup_line_regex ]]; then
 
@@ -103,10 +107,10 @@ for entry in $src_entries; do
 
 					# Find all files/directories matching the selector and select_type.
 					# More about this type of loop at: https://stackoverflow.com/a/9612114/6835329
-					find $src_dir/$entry -type $select_type -wholename "$selector" -not -name ".setup" -print0 | while read -d $'\0' sub_entry_path
+					find "$src_dir/$entry" -type "$select_type" -wholename "$selector" -not -name ".setup" -print0 | while read -d $'\0' sub_entry_path
 					do
 						# Now we need to link each file/directory to the dist_directory
-						sub_entry_name=$(basename $sub_entry_path)
+						sub_entry_name=$(basename "$sub_entry_path")
 						if [[ -d $sub_entry_path ]]; then
 							echo -e "${tab}Linking ${yellow}${bold}$sub_entry_name${reset} to ${yellow}${bold}$dist_dir${reset}"
 							ln -sf "$sub_entry_path" "$dist_dir"
@@ -115,13 +119,14 @@ for entry in $src_entries; do
 							ln -sf "$sub_entry_path" "$dist_dir/$sub_entry_name"
 						fi
 					done
-				else
+				elif [[ ! $line =~ $comment_line_regex ]]; then
+					# Allows comments but other things are considered errors
 					echo -e "${tab}${red}${bold}ERROR: Invalid '.setup' file, terminating setup...${reset}"
 					echo
 					exit 1
 				fi
 
-			done < $src_dir/$entry/.setup
+			done < "$src_dir/$entry/.setup"
 
 			echo -e "${tab}${green}${bold}Linking successful.${reset}"
 
