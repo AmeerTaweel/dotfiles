bf2d7898aa062b9e045a077b4904e50ecd0953f4
Ameer Taweel <ameertaweel2002@gmail.com>
2022-01-21 01:57:08 +0300

    refactor: nvim globals

--- nvim/.config/nvim/lua/globals.lua
+++ nvim/.config/nvim/lua/globals.lua
@@ -11,9 +11,7 @@ _G.exec.vimscript = vim.cmd
 -- Invokes a Vim function or a user function
 _G.fn = vim.fn
 
--- +------------+
--- | ## Options |
--- +------------+
+-- {{ Options }}
 
 _G.options = {}
 _G.options.global = vim.go
@@ -21,48 +19,7 @@ _G.options.window = vim.wo
 _G.options.buffer = vim.bo
 _G.options.object = vim.opt
 
--- +--------------+
--- | ## Variables |
--- +--------------+
+-- {{ Variables }}
 
 _G.variables = {}
 _G.variables.global = vim.g
-
--- +----------------+
--- | ## Key Mapping |
--- +----------------+
-
-_G.setKeymap = vim.api.nvim_set_keymap
-
--- ### Modes
-_G.mode = {}
-_G.mode.normal = "n"
-_G.mode.visual = "v"
-_G.mode.insert = "i"
-_G.mode.select = "s"
-
--- ### Keys
-_G.key = {}
-_G.key.leader = "<leader>"
-_G.key.tab = "<tab>"
-_G.key.shiftTab = "<s-tab>"
-_G.key.ctrlN = "<c-n>"
-_G.key.ctrlP = "<c-p>"
-
--- +--------+
--- | ## Lua |
--- +--------+
-
--- ### List-Like Tables
-_G.list = {}
-_G.list.contains = vim.tbl_contains
-
--- ### Strings
--- _G.string is already defined in the language
-_G.string.split = vim.split
-
--- +-----------+
--- | ## Others |
--- +-----------+
-
-_G.replaceTermCodes = vim.api.nvim_replace_termcodes
