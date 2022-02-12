b0ab5e8cd23948a5c8daff59779331b3231e392c
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-12 16:20:42 +0300

    feat: Use the official fixed version of tmux-fzf

--- nixos/user/labmem001/tmux.nix
+++ nixos/user/labmem001/tmux.nix
@@ -15,34 +15,17 @@ let
 			sha256 = "htnnjhFUNqoLszSVkpeAOsujtScCQ3sp9XDAOydG2XQ=";
 		};
 	};
-
-	customTmuxPackages.tmux-fzf = pkgs.tmuxPlugins.mkTmuxPlugin {
-		pluginName = "tmux-fzf";
-		rtpFilePath = "main.tmux";
-		version = "unstable-2020-12-07";
-		src = pkgs.fetchFromGitHub {
-			owner = "sainnhe";
-			repo = "tmux-fzf";
-			rev = "1801dd525b39154745ea668fb6916035023949e3";
-			sha256 = "e929Jqletmobp3WAR1tPU3pJuYTYVynxc5CvB80gig8=";
-		};
-		postInstall = ''
-			find $target -type f -print0 | xargs -0 sed -i -e 's|fzf |${pkgs.fzf}/bin/fzf |g'
-			find $target -type f -print0 | xargs -0 sed -i -e 's|sed |${pkgs.gnused}/bin/sed |g'
-			find $target -type f -print0 | xargs -0 sed -i -e 's|tput |${pkgs.ncurses}/bin/tput |g'
-		'';
-	};
 in {
     programs.tmux = {
         enable = true;
 		plugins = with pkgs.tmuxPlugins; [{
             plugin = customTmuxPackages.vim-tmux-navigator;
 		} {
-			plugin = customTmuxPackages.tmux-fzf;
+			plugin = tmux-fzf;
 			extraConfig = ''
 				TMUX_FZF_SESSION_FORMAT="#{session_windows} windows"
 				TMUX_FZF_OPTIONS="-p -w 62% -h 38% --cycle --bind 'tab:toggle-down,btab:toggle-up'"
-				bind-key "s" run-shell -b "${customTmuxPackages.tmux-fzf}/share/tmux-plugins/tmux-fzf/scripts/session.sh attach"
+				bind-key "s" run-shell -b "${tmux-fzf}/share/tmux-plugins/tmux-fzf/scripts/session.sh attach"
 			'';
 		}];
     };
