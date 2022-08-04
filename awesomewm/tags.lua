eab1f3bb749c869642a5822e3686f3f50e54ddab
Ameer Taweel <ameertaweel2002@gmail.com>
2022-08-04 20:50:35 +0300

    feat: reduce awesomewm tags

--- awesomewm/tags.lua
+++ awesomewm/tags.lua
@@ -4,12 +4,12 @@ local M = {
 	competitive_programming = "3 CPRG",
 	game = "4 GAME",
 	communication = "5 COMM",
-	turkey = "6 TURK",
+	machine_learning = "6 MLRN",
 	health = "7 HLTH",
 	tmp_internship = "8 ISHP",
 	cyber_security = "9 CSEC",
 	common_lisp = "10 LISP",
-	parallel_programming = "11 PPRG"
+	erp_max = "11 ERPX"
 }
 
 M.list = {
@@ -18,12 +18,12 @@ M.list = {
 	M.competitive_programming,
 	M.game,
 	M.communication,
-	M.turkey,
+	M.machine_learning,
 	M.health,
 	M.tmp_internship,
 	M.cyber_security,
 	M.common_lisp,
-	M.parallel_programming
+	M.erp_max
 }
 
 return M
