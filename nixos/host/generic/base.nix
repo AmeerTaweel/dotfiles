de91eed186b767476e0f85a0e37f1133cd97710c
Ameer Taweel <ameertaweel2002@gmail.com>
2022-12-30 17:18:31 +0300

    feat: enable udisks2 to automount storage devices

--- nixos/host/generic/base.nix
+++ nixos/host/generic/base.nix
@@ -28,6 +28,9 @@
 
 	services.upower.enable = true;
 
+	# DBus service for applications to query and manipulate storage devices.
+	services.udisks2.enable = true;
+
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
 	# on your system were taken. It‘s perfectly fine and recommended to leave
