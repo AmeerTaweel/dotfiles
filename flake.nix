0144e6a8593b8763663a9705a5465dc5ecf16913
Ameer Taweel <ameertaweel2002@gmail.com>
2023-02-10 23:57:34 +0300

    refactor: clean flake inputs section

--- flake.nix
+++ flake.nix
@@ -9,11 +9,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
+    # Nix Base-16 Theming
     nix-colors.url = "github:misterio77/nix-colors";
 
     # Music Downloader
