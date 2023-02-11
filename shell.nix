f130de38b6332c4c7abb9f81100b2a6d91383330
Ameer Taweel <ameertaweel2002@gmail.com>
2023-02-11 05:28:13 +0300

    doc: fix comment in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 # Shell for bootstrapping flake-enabled nix and home-manager
-# You can enter it through 'nix develop' or (legacy) 'nix-shell'
+# You can enter it through `nix develop` or (legacy) `nix-shell`
 {pkgs ? (import ./nixpkgs.nix) {}}: {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
