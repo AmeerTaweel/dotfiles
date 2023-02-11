20c401a5936041232b3874790c352bc4398ba662
Ameer Taweel <ameertaweel2002@gmail.com>
2023-02-11 05:30:07 +0300

    doc: improve comments in overlays file

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
-# This file defines overlays
+# This file defines custom overlays
 {
-  # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  # This one brings our custom packages from the `pkgs` directory
+  additions = final: prev: import ../pkgs {pkgs = final;};
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
