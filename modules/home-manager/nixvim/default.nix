7ee5ee7b34af8997770df4c58f306a9e481b9a01
Ameer Taweel <ameertaweel2002@gmail.com>
2023-08-24 14:54:57 +0300

    refactor: split autocommands and autogroups to module

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -45,6 +45,8 @@ in {
   imports = [
     inputs.nix-colors.homeManagerModules.default
     inputs.nixvim.homeManagerModules.nixvim
+
+    ./autocmd.nix
   ];
 
   home.sessionVariables = lib.mkIf (builtins.elem params.editor ["nvim" "neovim"]) {
@@ -141,57 +143,6 @@ in {
         tmux = "tmux";
       };
     };
-    autoGroups = {
-      AUTO_REBALANCE.clear = true;
-      DISABLE_AUTO_COMMENT.clear = true;
-      SMART_LINE_NUMBERS.clear = true;
-      DISABLE_NETRW_LIST.clear = true;
-      DISABLE_AUTO_FOLD.clear = true;
-    };
-    autoCmd = [
-      {
-        # Automatically re-balance windows on resize
-        group = "AUTO_REBALANCE";
-        event = ["VimResized"];
-        pattern = ["*"];
-        command = "wincmd =";
-      }
-      {
-        # Disable auto-commenting
-        group = "DISABLE_AUTO_COMMENT";
-        event = ["FileType"];
-        pattern = "*";
-        command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o";
-      }
-      {
-        # Make line numbers absolute when in insert mode and on buffer leaving
-        group = "SMART_LINE_NUMBERS";
-        event = ["BufLeave" "FocusLost" "InsertEnter"];
-        pattern = "*";
-        command = "setlocal norelativenumber";
-      }
-      {
-        # Restore relative line numbers when leaving insert move or entering buffer
-        group = "SMART_LINE_NUMBERS";
-        event = ["BufEnter" "FocusGained" "InsertLeave"];
-        pattern = "*";
-        command = "setlocal relativenumber";
-      }
-      {
-        # Disable annoying folds when opening a file
-        group = "DISABLE_AUTO_FOLD";
-        event = ["BufWinEnter"];
-        pattern = "*";
-        command = "normal zR";
-      }
-      {
-        # Disable whitespace character hints in netrw
-        group = "DISABLE_NETRW_LIST";
-        event = ["FileType"];
-        pattern = "netrw";
-        command = "setlocal nolist";
-      }
-    ];
     colorschemes.${nvimTheme.${config.colorScheme.slug}.name}.enable = true;
     maps = {
       normal = {
