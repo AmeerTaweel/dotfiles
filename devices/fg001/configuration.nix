e2829fd148772b070587e8ba10e43bdb8fdb0eb7
Ameer Taweel <ameertaweel2002@gmail.com>
2023-09-30 17:28:29 +0300

    feat: install steam

--- devices/fg001/configuration.nix
+++ devices/fg001/configuration.nix
@@ -43,6 +43,12 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
