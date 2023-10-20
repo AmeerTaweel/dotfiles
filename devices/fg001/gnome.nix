865f294883a18f1aacba7c5cd403052a3b5710e8
Ameer Taweel <ameertaweel2002@gmail.com>
2023-10-20 18:43:08 +0300

    feat: don't enable gdm in gnome specialization

--- devices/fg001/gnome.nix
+++ devices/fg001/gnome.nix
@@ -3,7 +3,6 @@
     system.nixos.tags = ["gnome-enabled"];
 
     # Enable the GNOME Desktop Environment
-    services.xserver.displayManager.gdm.enable = lib.mkForce true;
     services.xserver.desktopManager.gnome.enable = lib.mkForce true;
   };
 }
