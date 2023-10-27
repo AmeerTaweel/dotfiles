459c3ecadee46b53ac90204a591e9c115081adc8
Ameer Taweel <ameertaweel2002@gmail.com>
2023-10-28 02:44:40 +0300

    feat: split gnome config to separate module

--- devices/fg001/configuration.nix
+++ devices/fg001/configuration.nix
@@ -1,5 +1,6 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
+    ./gnome.nix
     ./hardware.nix
     ./nvidia.nix
     ./steam.nix
@@ -13,41 +14,6 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable the X11 windowing system
-  services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-console
-    gnome-photos
-    gnome-tour
-  ]) ++ (with pkgs.gnome; [
-    epiphany # web browser
-    evince # document viewer
-    geary # email reader
-    gedit # text editor
-    gnome-calendar
-    gnome-characters
-    gnome-contacts
-    gnome-maps
-    gnome-music
-    gnome-terminal
-    gnome-weather
-    totem # video player
-    yelp # help viewer
-  ]);
-  environment.systemPackages = with pkgs; [
-    gnome.gnome-tweaks
-
-    gnomeExtensions.hibernate-status-button # Hibernate in Power Options
-    gnomeExtensions.appindicator # System Tray
-    gnomeExtensions.pano # Clipboard Manager
-    gnomeExtensions.just-perfection
-  ];
-  # Ensure gnome-settings-daemon udev rules are enabled
-  services.udev.packages = [pkgs.gnome.gnome-settings-daemon];
 
   # System-wide packages
   # environment.systemPackages = [ ];
---
+++ devices/fg001/gnome.nix
@@ -0,0 +1,46 @@
+{pkgs, ...}: {
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Exclude default GNOME packages
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-console
+      gnome-photos
+      gnome-tour
+    ])
+    ++ (with pkgs.gnome; [
+      epiphany # web browser
+      evince # document viewer
+      geary # email reader
+      gedit # text editor
+      gnome-calendar
+      gnome-characters
+      gnome-contacts
+      gnome-maps
+      gnome-music
+      gnome-terminal
+      gnome-weather
+      totem # video player
+      yelp # help viewer
+    ]);
+
+  # Install GNOME extensions
+  environment.systemPackages =
+    (with pkgs.gnome; [
+      gnome-tweaks
+    ])
+    ++ (with pkgs.gnomeExtensions; [
+      hibernate-status-button # Hibernate in Power Options
+      appindicator # System Tray
+      pano # Clipboard Manager
+      just-perfection
+    ]);
+
+  # Ensure gnome-settings-daemon udev rules are enabled
+  services.udev.packages = [pkgs.gnome.gnome-settings-daemon];
+}
