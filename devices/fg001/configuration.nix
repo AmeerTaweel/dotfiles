cd5372c330df61867b01a2569b92925770367d53
Ameer Taweel <ameertaweel2002@gmail.com>
2023-10-27 17:43:41 +0300

    feat: exclude unused gnome applications

--- devices/fg001/configuration.nix
+++ devices/fg001/configuration.nix
@@ -19,6 +19,24 @@
   # Enable the GNOME Desktop Environment
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-calendar
+    gnome-characters
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-terminal
+    gnome-weather
+    totem # video player
+    yelp # help viewer
+  ]);
   environment.systemPackages = with pkgs; [
     gnome.gnome-tweaks
 
