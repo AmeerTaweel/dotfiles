013a2fbba614e8f18a502ecf85eef3e9c81e6dc5
Ameer Taweel <ameertaweel2002@gmail.com>
2024-02-01 18:15:33 +0300

    feat: set default font for mpv subtitles

--- modules/home-manager/video-players/mpv.nix
+++ modules/home-manager/video-players/mpv.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
+  imports = [../fonts.nix];
+
   programs.mpv = {
     enable = true;
     scripts = [pkgs.mpvScripts.mpris];
+    config = {
+      sub-font = "Dubai";
+    };
   };
 }
