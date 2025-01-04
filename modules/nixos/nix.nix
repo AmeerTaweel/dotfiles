cc4247e4abb50a29543b8a649fe46c3db6b4ab5a
Ameer Taweel <ameertaweel2002@gmail.com>
2025-01-04 22:43:00 +0200

    nix: enable auto-allocate-uids experimental feature

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -15,10 +15,13 @@
 
     settings = {
       # Enable experimental features (flakes, new `nix` commands, and content-addressed nix)
-      experimental-features = "nix-command flakes ca-derivations";
+      experimental-features = "nix-command flakes ca-derivations auto-allocate-uids";
 
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+
+      # Automatically pick UIDs for builds, rather than creating nixbld* user accounts
+      auto-allocate-uids = true;
     };
 
     # Auto garbage-collection to save disk space
