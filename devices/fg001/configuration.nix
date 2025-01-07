e16d904ee46b7fe29ed4f1631179e0099c7b900a
Ameer Taweel <ameertaweel2002@gmail.com>
2025-01-07 02:00:16 +0200

    configure tailscale and user passwords

--- devices/fg001/configuration.nix
+++ devices/fg001/configuration.nix
@@ -1,15 +1,19 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware.nix
+    ./impermanence.nix
     ./headless.nix
     ./nvidia.nix
 
-    ./modules/nixos/adb.nix
-    ./modules/nixos/core.nix
-    ./modules/nixos/gnome.nix
-    ./modules/nixos/kumech.nix
-    ./modules/nixos/nix.nix
-    ./modules/nixos/nix-index.nix
+    ../../modules/nixos/core.nix
+    ../../modules/nixos/virtualization/virt-manager.nix
+    ../../modules/nixos/virtualization/virtual-box.nix
+    ../../modules/nixos/virtualization/docker.nix
+    ../../modules/nixos/gnome.nix
+    ../../modules/nixos/nix.nix
+    ../../modules/nixos/nix-index.nix
+    ../../modules/nixos/teamviewer.nix
+    ../../modules/nixos/adb.nix
   ];
 
   # Bootloader
@@ -18,4 +22,56 @@
 
   # System-wide packages
   # environment.systemPackages = [ ];
+
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/run/secrets/tailscale_key";
+  };
+
+  # mDNS Config
+  services.resolved.enable = true;
+  networking.networkmanager.connectionConfig."connection.mdns" = 2;
+  services.avahi.enable = true;
+
+  # echo "PASSWORD" | mkpasswd -s
+  users.users.labmem001.hashedPasswordFile = "/persist-ssd/run/secrets/labmem001-hashed-pwd";
+  users.users.root.hashedPasswordFile = "/persist-ssd/run/secrets/root-hashed-pwd";
+
+  # Configure Filesystem Permissions
+  systemd.tmpfiles.settings = {
+    "archive" = {
+      "/home/labmem001/archive/operating-systems/*" = {
+        Z = {
+          group = "users";
+          mode = "0444";
+          user = "labmem001";
+        };
+        h = {
+          argument = "+i";
+        };
+      };
+      "/home/labmem001/archive/papers/*" = {
+        Z = {
+          group = "users";
+          mode = "0444";
+          user = "labmem001";
+        };
+        h = {
+          argument = "+i";
+        };
+      };
+      "/run/secrets" = {
+        Z = {
+          group = "root";
+          mode = "0751";
+          user = "root";
+        };
+      };
+      "/run/secrets/*" = {
+        h = {
+          argument = "+i";
+        };
+      };
+    };
+  };
 }
