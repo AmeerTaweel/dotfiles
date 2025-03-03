75791a9372577eba1a1dceff4d20064bb58056f1
Ameer Taweel <ameertaweel2002@gmail.com>
2025-03-03 12:00:52 +0200

    update git settings
    based on: https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -4,9 +4,26 @@
     userName = params.name;
     userEmail = params.email;
     extraConfig = {
-      init = {
-        defaultBranch = "master";
+      init.defaultBranch = "master";
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
       };
+      push.autoSetupRemote = true;
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      merge.conflictstyle = "zdiff3";
+      pull.rebase = true;
     };
   };
 }
