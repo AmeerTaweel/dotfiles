f9ac548c5488164a3d439fc6e82358c519c960aa
Ameer Taweel <ameertaweel2002@gmail.com>
2025-03-17 00:16:55 +0200

    style: nix fmt

--- devices/fg001/borgmatic.nix
+++ devices/fg001/borgmatic.nix
@@ -1,38 +1,63 @@
-{params, pkgs, ...}: let
-  mkBorgmaticNotification = {title, urgency ? "NORMAL"}: ''
+{
+  params,
+  pkgs,
+  ...
+}: let
+  mkBorgmaticNotification = {
+    title,
+    urgency ? "NORMAL",
+  }: ''
     export $(${pkgs.coreutils}/bin/cat /proc/$(${pkgs.procps}/bin/pgrep "gnome-session" -u "${params.username}")/environ | ${pkgs.coreutils}/bin/grep -z '^DBUS_SESSION_BUS_ADDRESS=')
     ${pkgs.libnotify}/bin/notify-send -u "${urgency}" "${title}" "Run sudo journalctl _SYSTEMD_USER_UNIT=borgmatic.service for details"
   '';
 
   mkBorgmaticHooks = repo: {
-    before_backup  = [(mkBorgmaticNotification {
-      title = "${repo}: Backup Started";
-    })];
-    before_prune  = [(mkBorgmaticNotification {
-      title = "${repo}: Prune Started";
-    })];
-    before_compact = [(mkBorgmaticNotification {
-      title = "${repo}: Compact Started";
-    })];
-    before_check = [(mkBorgmaticNotification {
-      title = "${repo}: Check Started";
-    })];
-    after_backup = [(mkBorgmaticNotification {
-      title = "${repo}: Backup Finished";
-    })];
-    after_prune = [(mkBorgmaticNotification {
-      title = "${repo}: Prune Finished";
-    })];
-    after_compact = [(mkBorgmaticNotification {
-      title = "${repo}: Compact Finished";
-    })];
-    after_check = [(mkBorgmaticNotification {
-      title = "${repo}: Check Finished";
-    })];
-    on_error = [(mkBorgmaticNotification {
-      title = "${repo}: ERROR";
-      urgency = "CRITICAL";
-    })];
+    before_backup = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Backup Started";
+      })
+    ];
+    before_prune = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Prune Started";
+      })
+    ];
+    before_compact = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Compact Started";
+      })
+    ];
+    before_check = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Check Started";
+      })
+    ];
+    after_backup = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Backup Finished";
+      })
+    ];
+    after_prune = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Prune Finished";
+      })
+    ];
+    after_compact = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Compact Finished";
+      })
+    ];
+    after_check = [
+      (mkBorgmaticNotification {
+        title = "${repo}: Check Finished";
+      })
+    ];
+    on_error = [
+      (mkBorgmaticNotification {
+        title = "${repo}: ERROR";
+        urgency = "CRITICAL";
+      })
+    ];
   };
 in {
   services.borgmatic = {
