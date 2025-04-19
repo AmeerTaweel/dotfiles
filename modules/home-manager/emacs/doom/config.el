67e2b2c0d55a10087014487c197e8401375c949c
Ameer Taweel <ameertaweel2002@gmail.com>
2025-04-19 14:40:53 +0300

    emacs: make line number column width static

--- modules/home-manager/emacs/doom/config.el
+++ modules/home-manager/emacs/doom/config.el
@@ -38,6 +38,7 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(setq display-line-numbers-width-start t)
 
 (use-package! org
   ; Code that runs before the package is loaded
