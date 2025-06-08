8a38893a0211697c0905f73eeccb6a7de1cdc7dd
Ameer Taweel <ameertaweel2002@gmail.com>
2025-06-09 00:20:32 +0300

    scrutiny service: init

---
+++ devices/vg001/containers/scrutiny.nix
@@ -0,0 +1,34 @@
+{
+  host-address,
+  local-address,
+  port,
+  state-version,
+  ...
+}: {
+  autoStart = true;
+  privateNetwork = true;
+  hostAddress = host-address;
+  localAddress = local-address;
+  config = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: {
+    services.scrutiny = {
+      enable = true;
+      collector.enable = false;
+      influxdb.enable = true;
+      settings.web.listen.host = "0.0.0.0";
+      settings.web.listen.port = port;
+    };
+    networking.firewall.allowedTCPPorts = [port];
+
+    # Use systemd-resolved inside the container
+    # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+    networking.useHostResolvConf = lib.mkForce false;
+    services.resolved.enable = true;
+
+    system.stateVersion = state-version;
+  };
+}
